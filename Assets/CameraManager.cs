b51e52e2541d4261508bba09217696664bf8f2bb
XYZetDelta <zetilsagames@gmail.com>
2025-08-22 13:53:05 +0700

    Lighting & PostProcess

--- Assets/CameraManager.cs
+++ Assets/CameraManager.cs
@@ -7,16 +7,6 @@ public class CameraManager : MonoBehaviour
     [SerializeField] private GameObject[] UI;
     [SerializeField] private CanvasGroup cg;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void Move(int state)
     {
