f75c97375df0c8a6a8b1bbe700d06419129458de
Riffat45 <riffatriz45@gmail.com>
2025-08-24 08:07:21 +0700

    24/08/25
    Camera Update
    Updated Shader
    Room Adjustment
    Light & Reflection Adjustment

--- Assets/CameraManager.cs
+++ Assets/CameraManager.cs
@@ -6,60 +6,39 @@ public class CameraManager : MonoBehaviour
     [SerializeField] private Transform[] paths;
     [SerializeField] private GameObject[] UI;
     [SerializeField] private CanvasGroup cg;
+
+    private bool onClock;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public void Move(int state)
     {
         cg.blocksRaycasts = false;
-        if (state == 0)
+        if (state != 1 || state != 2 || state != 7)
         {
-            transform.DOMove(paths[0].position, .5f).SetEase(Ease.InOutCubic);
-            transform.DORotateQuaternion(paths[0].rotation, .5f).SetEase(Ease.InOutCubic).OnComplete(() =>
-            {
-                UI[0].SetActive(true);
-                UI[1].SetActive(false);
-                UI[2].SetActive(false);
-                UI[3].SetActive(false);
-                cg.blocksRaycasts = true;
-            });
+            transform.DOMove(paths[state].position, .5f).SetEase(Ease.InOutCubic);
         }
-        else if (state == -1)
+        if (state == 7)
         {
-            transform.DORotateQuaternion(paths[2].rotation, .5f).SetEase(Ease.InOutCubic).OnComplete(() =>
+            if (onClock == false)
             {
-                UI[0].SetActive(false);
-                UI[1].SetActive(true);
-                UI[2].SetActive(false);
-                UI[3].SetActive(false);
-                cg.blocksRaycasts = true;
-            });
+                onClock = true;
+                DOTween.Kill(transform);
+                transform.DORotateQuaternion(paths[state].rotation, .5f).SetEase(Ease.OutCubic);
+            }
         }
-        else if (state == 1)
+        else if(state != 7)
         {
-            transform.DORotateQuaternion(paths[1].rotation, .5f).SetEase(Ease.InOutCubic).OnComplete(() =>
-            {
-                UI[0].SetActive(false);
-                UI[1].SetActive(false);
-                UI[2].SetActive(true);
-                UI[3].SetActive(false);
-                cg.blocksRaycasts = true;
-            });
-        }
-        else if (state == 2)
-        {
-            transform.DOMove(paths[3].position, .5f).SetEase(Ease.InOutCubic);
-            transform.DORotateQuaternion(paths[3].rotation, .5f).SetEase(Ease.InOutCubic).OnComplete(() =>
-            {
-                UI[0].SetActive(false);
-                UI[1].SetActive(false);
-                UI[2].SetActive(false);
-                UI[3].SetActive(true);
-                cg.blocksRaycasts = true;
-            });
-        }
-        else
-        {
-            cg.blocksRaycasts = true;
+
+                onClock = false;
+                transform.DORotateQuaternion(paths[state].rotation, .5f).SetEase(Ease.InOutCubic).OnComplete(() =>
+                {
+                    foreach (GameObject ui in UI)
+                    {
+                        ui.SetActive(false);
+                    }
+                    UI[state].SetActive(true);
+                    cg.blocksRaycasts = true;
+                });
         }
     }
 }
